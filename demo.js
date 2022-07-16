f88fdf3f205c5f1e4860cc744cf3f1fd4460cbfb
vanhuynh123 <81578158+vanhuynh123@users.noreply.github.com>
2022-07-16 09:36:23 +0700

    Add file

---
+++ demo.js
@@ -0,0 +1,6 @@
+let inputwidth= prompt("enter the width");
+        let inputheight=prompt("enter the height");
+        let width=parseInt(inputwidth);
+        let height= parseInt(inputheight);
+        let area = width*height;
+        document.write("The area is:"+area);
